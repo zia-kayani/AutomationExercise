685d0b7ed35180a0b7cde98a548f72cc034e1c62
zia kayani <ziakayani.official@gmail.com>
2026-04-20 14:19:09 +0500

    Modify ui workflow and root confest file

--- conftest.py
+++ conftest.py
@@ -1,5 +1,43 @@
+# import pytest
+# from framework.common.config.config import Config
+
+# @pytest.fixture
+# def context(browser):
+#     context = browser.new_context()
+#     yield context
+#     context.close()
+
+# @pytest.fixture
+# def page(context):
+#     page = context.new_page()
+
+#     if not Config.BASE_URL:
+#         raise ValueError("BASE_URL is not set in environment variables")
+#     page.goto(Config.BASE_URL)
+
+
+#     yield page
+#     page.close()
+
 import pytest
+from playwright.sync_api import sync_playwright
 from framework.common.config.config import Config
+
+
+@pytest.fixture(scope="session")
+def playwright_instance():
+    with sync_playwright() as p:
+        yield p
+
+
+@pytest.fixture(scope="session")
+def browser(playwright_instance):
+    browser = playwright_instance.chromium.launch(
+        headless=Config.HEADLESS   
+    )
+    yield browser
+    browser.close()
+
 
 @pytest.fixture
 def context(browser):
@@ -7,16 +45,15 @@ def context(browser):
     yield context
     context.close()
 
+
 @pytest.fixture
 def page(context):
     page = context.new_page()
 
     if not Config.BASE_URL:
         raise ValueError("BASE_URL is not set in environment variables")
-    page.goto(Config.BASE_URL)
 
+    page.goto(Config.BASE_URL)
 
     yield page
     page.close()
-
-    
